0c1a11184f4d00faaa1d6d3d70b98ddb1fea3af8
kristinreddington <kristinreddington@gmail.com>
2019-05-27 18:43:58 -0400

    add redux, react-redux, reselect and create redux store

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,19 +1,21 @@
-import React from "react"
-import PropTypes from "prop-types"
-import HomePage from './HomePage'; 
+import React from 'react';
+import PropTypes from 'prop-types';
+import Provider from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import HomePage from './HomePage';
 
 class App extends React.Component {
   render () {
     return (
-      <React.Fragment>
+      <Provider store={store}>
         <BrowserRouter>
-        <Switch> 
-          <Route exact path="/" render={() => ("Home!")} />
-          <Route path="/hello" render={() => <HomePage greeting="Friend"/>} />
-        </Switch>
+          <Switch>
+            <Route exact path="/" render={() => ("Home!")} />
+            <Route path="/hello" render={() => <HomePage greeting="Friend"/>} />
+          </Switch>
         </BrowserRouter>
-      </React.Fragment>
+      </Provider>
     );
   }
 }
