79dd20daf1a5b99943e13fac3db43c3cb7c39344
Kristin Reddington <Kristin.Reddington@ibm.com>
2019-07-03 11:03:35 -0400

    add contact page text

--- app/javascript/components/About.js
+++ app/javascript/components/About.js
@@ -4,11 +4,11 @@ class About extends Component {
     render() {
         return(
             <div id="about-block">
-                <p id="about">With an eye for detail and keen sense of all things digital,<br/>
+                <p id="about">With an eye for detail and keen sense for all things digital,<br/>
                 Kristin is a modern entrepreneurial problem solver. In addition to being <br/> 
                 a creative, passionate web developer, Kristin has successfully launched <br />
-                two online marketing businesses, where she's used cutting-edge SEO, paid ad,<br />
-                and branding techniques to grab clients' most valuable resource:<br />
+                two online marketing businesses, where she's used cutting-edge branding, SEO <br />
+                and paid ad techniques to grab clients' most valuable resource:<br />
                 <i><strong style={{}}>attention.</strong></i><br />
                 Kristin is a conscious capitalist, believing in positively changing <br />
                 lives and empowering the individual through free market to elevate humanity. <br />
--- app/javascript/components/Contact.js
+++ app/javascript/components/Contact.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react'; 
+
+import './Contact.css'; 
 class Contact extends Component {
     render() {
         return(
-            <div>Contact</div>
+            <div id="contact-block">
+                <p id="contact">
+                    For all business inquiries: kristinreddington@gmail.com* <br /> <br /><br />
+
+
+                    <i id="disclaimer">*Kristin believes in the philosophy of doing purposeful work, <br />
+                    which involves outsourcing and automating social media, <br />
+                    doing uninterrupted deep work, and checking email once <br />
+                    per week. Please allow 1-5 business days for a response <br />
+                    to your inquiry. Once a client, you will have access to a <br />
+                    phone number for your convenience.</i>
+
+                </p>
+            </div>
+
+            
         )
     }
 
