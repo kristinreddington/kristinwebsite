eef67ea0d3b99dba1b4e67287f6ad457ea82e6c8
Kristin Reddington <Kristin.Reddington@ibm.com>
2019-07-03 11:38:18 -0400

    add about and contact css

--- app/javascript/components/About.js
+++ app/javascript/components/About.js
@@ -7,7 +7,7 @@ class About extends Component {
                 <p id="about">With an eye for detail and keen sense for all things digital,<br/>
                 Kristin is a modern entrepreneurial problem solver. In addition to being <br/> 
                 a creative, passionate web developer, Kristin has successfully launched <br />
-                two online marketing businesses, where she's used cutting-edge branding, SEO <br />
+                two online marketing businesses, where she's used cutting-edge branding, SEO and<br />
                 and paid ad techniques to grab clients' most valuable resource:<br />
                 <i><strong style={{}}>attention.</strong></i><br />
                 Kristin is a conscious capitalist, believing in positively changing <br />
